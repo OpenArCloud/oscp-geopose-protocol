7d469f9f60081e6eac8503ea3d0f1e155604253f
soeroesg <gabor.soros@gmail.com>
2024-01-15 21:50:30 +0100

    added geopose_utils.py; fixed height problem in geopose_utils.h

--- cpp/oscp-gpp/include/oscp-gpp/geopose_utils.h
+++ cpp/oscp-gpp/include/oscp-gpp/geopose_utils.h
@@ -17,12 +17,14 @@
 
 // The mathematical formulas were adapted from Augmented City
 // https://developer.augmented.city/doc#section/GeoPose/How-to-Convert-to-Cartesian-Coordinate-System
+//
+// The general formulas can be found in https://en.wikipedia.org/wiki/Geographic_coordinate_conversion
 
 static const double a = 6378137.0000; // Earth radius in meters
 static const double b = 6356752.3142;  // Earth semiminor in meters
 static const double pi = std::acos(-1);
 static const double f = (a - b) / a;
-static const double e_sq = f * (2 - f);
+static const double e_sq = f * (2 - f); // first eccentricity squared = (a2 - b2) / a2
 
 double degrees_to_radians(double degrees) {
     return degrees * (pi / 180.0);
@@ -32,8 +34,7 @@ double radians_to_degrees(double radians) {
     return radians * (180 / pi);
 }
 
-// (lat, lon) in degrees
-// h in meters
+// lat, lon in degrees, h in meters
 void geodetic_to_ecef(double lat, double lon, double h, double* x, double* y, double* z) {
     assert(x != nullptr);
     assert(y != nullptr);
@@ -41,17 +42,17 @@ void geodetic_to_ecef(double lat, double lon, double h, double* x, double* y, do
 
     double lamb = degrees_to_radians(lat);
     double phi = degrees_to_radians(lon);
-    double s = std::sin(lamb);
-    double N = a / std::sqrt(1 - e_sq * s * s);
 
     double sin_lambda = std::sin(lamb);
     double cos_lambda = std::cos(lamb);
     double sin_phi = std::sin(phi);
     double cos_phi = std::cos(phi);
 
-    *x = (h + N) * cos_lambda * cos_phi;
-    *y = (h + N) * cos_lambda * sin_phi;
-    *z = (h + (1 - e_sq) * N) * sin_lambda;
+    double nu = a / std::sqrt(1 - e_sq * sin_lambda * sin_lambda);
+
+    *x = (h + nu) * cos_lambda * cos_phi;
+    *y = (h + nu) * cos_lambda * sin_phi;
+    *z = (h + (1 - e_sq) * nu) * sin_lambda;
 }
 
 void ecef_to_enu(double x, double y, double z, double lat0, double lon0, double h0, double* xEast, double* yNorth, double* zUp) {
@@ -61,17 +62,17 @@ void ecef_to_enu(double x, double y, double z, double lat0, double lon0, double 
 
     double lamb = degrees_to_radians(lat0);
     double phi = degrees_to_radians(lon0);
-    double s = std::sin(lamb);
-    double N = a / std::sqrt(1 - e_sq * s * s);
 
     double sin_lambda = std::sin(lamb);
     double cos_lambda = std::cos(lamb);
     double sin_phi = std::sin(phi);
     double cos_phi = std::cos(phi);
 
-    double x0 = (h0 + N) * cos_lambda * cos_phi;
-    double y0 = (h0 + N) * cos_lambda * sin_phi;
-    double z0 = (h0 + (1 - e_sq) * N) * sin_lambda;
+    double nu = a / std::sqrt(1 - e_sq * sin_lambda * sin_lambda);
+
+    double x0 = (h0 + nu) * cos_lambda * cos_phi;
+    double y0 = (h0 + nu) * cos_lambda * sin_phi;
+    double z0 = (h0 + (1 - e_sq) * nu) * sin_lambda;
 
     double xd = x - x0;
     double yd = y - y0;
@@ -91,17 +92,17 @@ void enu_to_ecef(double xEast, double yNorth, double zUp, double lat0, double lo
 
     double lamb = degrees_to_radians(lat0);
     double phi = degrees_to_radians(lon0);
-    double s = std::sin(lamb);
-    double N = a / std::sqrt(1 - e_sq * s * s);
 
     double sin_lambda = std::sin(lamb);
     double cos_lambda = std::cos(lamb);
     double sin_phi = std::sin(phi);
     double cos_phi = std::cos(phi);
 
-    double x0 = (h0 + N) * cos_lambda * cos_phi;
-    double y0 = (h0 + N) * cos_lambda * sin_phi;
-    double z0 = (h0 + (1 - e_sq) * N) * sin_lambda;
+    double nu = a / std::sqrt(1 - e_sq * sin_lambda * sin_lambda);
+
+    double x0 = (h0 + nu) * cos_lambda * cos_phi;
+    double y0 = (h0 + nu) * cos_lambda * sin_phi;
+    double z0 = (h0 + (1 - e_sq) * nu) * sin_lambda;
 
     double t = cos_lambda * zUp - sin_lambda * yNorth;
 
@@ -116,51 +117,37 @@ void enu_to_ecef(double xEast, double yNorth, double zUp, double lat0, double lo
 
 // Convert from ECEF cartesian coordinates to
 // latitude, longitude and height (WGS84)
-void ecef_to_geodetic(double x, double y, double z, double* lat0, double* lon0, double* h0) {
-    assert(lat0 != nullptr);
-    assert(lon0 != nullptr);
-    assert(h0 != nullptr);
+void ecef_to_geodetic(double x, double y, double z, double* lat, double* lon, double* h) {
+    assert(lat != nullptr);
+    assert(lon != nullptr);
+    assert(h != nullptr);
 
-    double x2 = x * x;
-    double y2 = y * y;
-    double z2 = z * z;
+    // formula from http://www.movable-type.co.uk/scripts/latlong-os-gridref.html#cartesian-to-geodetic
+    const double e1_sq = 2*f - f*f; // 1st eccentricity squared = (a^2-b^2)/a^2
+    const double e2_sq = e1_sq / (1 - e1_sq); // 2nd eccentricity squared = (a^2-b^2)/b^2
+    const double p = std::sqrt(x*x + y*y); // distance from minor axis
+    const double R = std::sqrt(p*p + z*z); // polar radius
 
-    double e = std::sqrt(1 - (b / a) * (b / a));
-    double b2 = b * b;
-    double e2 = e * e;
-    double ep = e * (a / b);
-    double r = std::sqrt(x2 + y2);
-    double r2 = r * r;
-    double E2 = a * a - b * b;
-    double F = 54 * b2 * z2;
-    double G = r2 + (1 - e2) * z2 - e2 * E2;
-    double c = (e2 * e2 * F * r2) / (G * G * G);
-    double s = std::pow((1 + c + std::sqrt(c * c + 2 * c)), (1 / 3));
-    double P = F / (3 * pow((s + 1 / s + 1), 2) * G * G); //TODO: this line is probably wrong!
-    double Q = std::sqrt(1 + 2 * e2 * e2 * P);
-    double ro = -(P * e2 * r) / (1 + Q) + std::sqrt((a * a / 2) * (1 + 1 / Q) - (P * (1 - e2) * z2) / (Q * (1 + Q)) - P * r2 / 2);
-    double tmp = std::pow((r - e2 * ro), 2);
-    double U = std::sqrt(tmp + z2);
-    double V = std::sqrt(tmp + (1 - e2) * z2);
-    double zo = (b2 * z) / (a * V);
+    // parametric latitude (Bowring eqn.17)
+    const double tanBeta = (b*z)/(a*p) * (1 + e2_sq * b / R);
+    const double sinBeta = tanBeta / std::sqrt(1 + tanBeta * tanBeta);
+    const double cosBeta = sinBeta / tanBeta;
 
-    double height = U * (1 - b2 / (a * V));
+    // geodetic latitude (Bowring eqn.18)
+    const double latRad = std::isnan(cosBeta) ? 0 : std::atan2(z + e2_sq * b* sinBeta * sinBeta * sinBeta, p - e1_sq * a * cosBeta * cosBeta * cosBeta);
 
-    double lat = std::atan((z + ep * ep * zo) / r);
+    // longitude
+    const double lonRad = std::atan2(y, x);
 
-    double temp = std::atan(y / x);
-    double lon = 0.0;
-    if (x >= 0) {
-        lon = temp;
-    } else if (x < 0 && y >= 0) {
-        lon = pi + temp;
-    } else {
-        lon = temp - pi;
-    }
+    // height above ellipsoid (Bowring eqn.7)
+    const double sinLat = std::sin(latRad);
+    const double cosLat = std::cos(latRad);
+    const double nu = a / std::sqrt(1 - e1_sq * sinLat * sinLat); // length of the normal terminated by the minor axis
+    const double height = p * cosLat + z * sinLat - (a * a / nu);
 
-    *lat0 = lat / (pi / 180);
-    *lon0 = lon / (pi / 180);
-    *h0 = height;
+    *lat = latRad / (pi / 180);
+    *lon = lonRad / (pi / 180);
+    *h = height;
 }
 
 void geodetic_to_enu(double lat, double lon, double h, double lat_ref, double lon_ref, double h_ref, double* xEast, double* yNorth, double* zUp) {
@@ -172,13 +159,13 @@ void geodetic_to_enu(double lat, double lon, double h, double lat_ref, double lo
     ecef_to_enu(x, y, z, lat_ref, lon_ref, h_ref, xEast, yNorth, zUp);
 }
 
-void enu_to_geodetic(double xEast, double yNorth, double zUp, double lat_ref, double lon_ref, double h_ref, double* lat0, double* lon0, double* h0){
-    assert(lat0 != nullptr);
-    assert(lon0 != nullptr);
-    assert(h0 != nullptr);
+void enu_to_geodetic(double xEast, double yNorth, double zUp, double lat_ref, double lon_ref, double h_ref, double* lat, double* lon, double* h){
+    assert(lat != nullptr);
+    assert(lon != nullptr);
+    assert(h != nullptr);
     double x; double y; double z;
     enu_to_ecef(xEast, yNorth, zUp, lat_ref, lon_ref, h_ref, &x, &y, &z);
-    ecef_to_geodetic(x, y, z, lat0, lon0, h0);
+    ecef_to_geodetic(x, y, z, lat, lon, h);
 }
 
 #endif // _OSCP_GEOPOSE_UTILS_H_
