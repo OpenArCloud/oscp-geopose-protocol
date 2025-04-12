6915c75df3c55553b32ed5bd5cc5174365d3fd87
soeroesg <gabor.soros@gmail.com>
2025-04-12 14:13:35 +0200

    C++ client: remove OpenCV dependency, load image ourselves; add parameters VPS URL and port

--- cpp/oscp-gpp-demo/main_client.cpp
+++ cpp/oscp-gpp-demo/main_client.cpp
@@ -13,8 +13,8 @@
 
 #include <uuid.h>
 #include <nlohmann/json.hpp>
-#include <opencv2/core.hpp>
-#include <opencv2/imgcodecs.hpp>
+//#include <opencv2/core.hpp>
+//#include <opencv2/imgcodecs.hpp>
 
 #include <oscp-gpp/geoposeprotocol.h>
 #include <oscp-gpp/geoposeprotocol_json.h>
@@ -26,25 +26,66 @@
 #include <string>
 #include <vector>
 
+#include <fstream>
+
+namespace ImageUtils {
+    // code from here: https://codepal.ai/code-generator/query/PdT5LyrC/how-to-take-input-image-cpp-without-opencv-library
+
+    /**
+    * @brief Function to read an image file in binary mode and store its contents in a vector.
+    *
+    * @param filename The name of the image file to read.
+    * @return std::vector<uchar> A vector containing the binary data of the image.
+    * @throws std::runtime_error if the file cannot be opened or read.
+    */
+    std::vector<uint8_t> ReadImage(const std::string& filename) {
+        std::ifstream file(filename, std::ios::binary);
+
+        if (!file.is_open()) {
+            throw std::runtime_error("Error: Unable to open file.");
+        }
+
+        // Determine the size of the file
+        file.seekg(0, std::ios::end);
+        std::streampos fileSize = file.tellg();
+        file.seekg(0, std::ios::beg);
+
+        // Read the file content into a vector
+        std::vector<uint8_t> imageData(fileSize);
+        file.read((char*)(imageData.data()), fileSize);
+
+        file.close();
+
+        return imageData;
+    }
+}
+
 int main(int argc, char* argv[]) {
     try {
         std::cout << "Starting GPP Client..." << std::endl;
 
-        if (argc < 4) {
-            throw std::invalid_argument("Usage: oscp-gpp-client <IMAGE_PATH> <CAMERA_PARAMS_PATH> <GEOLOCATION_PARAMS_PATH>");
+        if (argc != 6) {
+            throw std::invalid_argument("Usage: oscp-gpp-client <VPS_URL> <VPS_PORT> <IMAGE_PATH> <CAMERA_PARAMS_PATH> <GEOLOCATION_PARAMS_PATH>");
         }
+        const int argIdxVpsUrl = 1;
+        const int argIdxVpsPort = 2;
+        const int argIdxImagePath = 3;
+        const int argIdxCameraParamsPath = 4;
+        const int argIdxGeolocationParamsPath = 5;
 
-        std::string myImagePath = argv[1];
+        std::string myVpsUrl = argv[argIdxVpsUrl];
+        std::string myVpsPort = argv[argIdxVpsPort];
+        std::cout << "VPS URL: " << myVpsUrl << ":" << myVpsPort << std::endl;
+        std::string myImagePath = argv[argIdxImagePath];
         std::cout << "Image path: " << myImagePath << std::endl;
-        std::string myCameraParamsPath = argv[2];
-
+        std::string myCameraParamsPath = argv[argIdxCameraParamsPath];
         std::cout << "Camera params path: " << myCameraParamsPath << std::endl;
         std::ifstream myCameraParamsFile(myCameraParamsPath);
         if (!myCameraParamsFile.is_open()) {
             throw std::invalid_argument("Could not open file " + myCameraParamsPath);
         }
         nlohmann::json myCameraParamsJson = json::parse(myCameraParamsFile);
-        std::string myGeolocationParamsPath = argv[3];
+        std::string myGeolocationParamsPath = argv[argIdxGeolocationParamsPath];
         std::cout << "Geolocation params path: " << myGeolocationParamsPath << std::endl;
         std::ifstream myGeolocationParamsFile(myGeolocationParamsPath);
         if (!myGeolocationParamsFile.is_open()) {
@@ -55,13 +96,16 @@ int main(int argc, char* argv[]) {
         oscp::CameraModel myCameraModel = oscp::cameraModelFromString(myCameraParamsJson["camera_model"]);
         // NOTE: camera params: fx, fy, cx, cy, k1, k2, p1, p2
         // Colmap and therefore also GPP ignores the 5th OpenCV coefficient
-        std::vector<float> myCameraModelParams = myCameraParamsJson["camera_params"];
+        const std::vector<float> myCameraModelParams = myCameraParamsJson["camera_params"];
+        int imgWidth = myCameraParamsJson["camera_width"];
+        int imgHeight = myCameraParamsJson["camera_height"];
         std::string myCameraSensorId = myCameraParamsJson["camera_id"];
 
         uint64_t myTimestamp =  std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now().time_since_epoch()).count();
         unsigned int myRequestCounter = 0;
 
-        // Load image
+        /*
+        // Load image with OpenCV
         cv::Mat img = cv::imread(myImagePath, cv::IMREAD_COLOR);
         if (img.empty()) {
             throw std::invalid_argument("Could not open file " + myImagePath);
@@ -73,6 +117,13 @@ int main(int argc, char* argv[]) {
             std::cout << errorMsg << std::endl;
             return -1;
         }
+        if (imgWidth != img.cols || imgHeight != img.rows) {
+            throw std::runtime_error("The loaded image size does not match the camera parameters");
+        };
+        */
+        // Alternatively, load JPEG image bytes
+        std::vector<uint8_t> imgJPEG = ImageUtils::ReadImage(myImagePath);
+
         // Encode JPEG to Base64
         std::string imgBase64 = oscp::base64_encode(imgJPEG.data(), imgJPEG.size());
 
@@ -96,8 +147,8 @@ int main(int argc, char* argv[]) {
         cameraReading.imageFormat = oscp::ImageFormat::JPG;
         cameraReading.imageOrientation = oscp::ImageOrientation(false, 0.0);
         cameraReading.sequenceNumber = myRequestCounter;
-        cameraReading.size[0] = img.cols;
-        cameraReading.size[1] = img.rows;
+        cameraReading.size[0] = imgWidth;
+        cameraReading.size[1] = imgHeight;
         cameraReading.sensorId = cameraSensor.id;
         cameraReading.timestamp = myTimestamp;
         oscp::CameraParameters cameraParameters;
@@ -120,7 +171,7 @@ int main(int argc, char* argv[]) {
         requestDataJsonNoImage["sensorReadings"]["cameraReadings"][0]["imageBytes"] = "<IMAGE_BASE64>";
         std::cout << "REQUEST JSON:" << std::endl << requestDataJsonNoImage << std::endl;
 
-        httplib::Client client("localhost", 8080);
+        httplib::Client client(myVpsUrl, std::stoi(myVpsPort));
 
         if (auto res = client.Post("/geopose", requestDataString, "application/json")) {
             if (res->status == 200) {
